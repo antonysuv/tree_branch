52fadcb4fbb1e1824b1f1952ad5ddbf9ae08bcae
Anton <antony.suvorov@gmail.com>
2022-07-31 20:49:58 -0400

    shell scripts

---
+++ shell_slurm_simulation_scripts/prepare_input.sh
@@ -0,0 +1,7 @@
+ls -v TRAIN/aln_*.fa
+ls -v TRAIN/aln_*.fa | xargs cat > TRAIN/TRAIN.fa
+ls -v TEST/aln_*.fa
+ls -v TEST/aln_*.fa | xargs cat > TEST/TEST.fa
+ls -v TEST/ml_*.tre
+ls -v TEST/ml_*.tre | xargs cat > TEST/ML_all.tre
+~/scripts/fasta2numeric.py --tr TRAIN/TRAIN.fa --te TEST/TEST.fa
---
+++ shell_slurm_simulation_scripts/produceTRAIN_alisim_iqtree_array_GTR.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+trees=$1
+len=$2
+
+n=$( cat $trees | wc -l )
+sed -n 1p "$trees" > fixed_tree.tre
+for i in `seq 1 $n`
+do 
+	sed -n "$i"p "$trees" > in_tree_"$i".tre
+done
+
+batch=alisim_iqtree_array.sh
+
+echo '#!/bin/bash
+#SBATCH --job-name=alisim
+#SBATCH -N 1
+#SBATCH --mem=1G
+#SBATCH -n 1
+#SBATCH -t 100:00:00
+#SBATCH --array=1-'"$n"':1000
+#SBATCH --requeue' >> $batch
+
+echo 'date
+range=$((${SLURM_ARRAY_TASK_ID}+999))
+for tr in `seq ${SLURM_ARRAY_TASK_ID} $range`
+do
+myrand=$((`shuf -i 0-4294967295 -n1`))
+file=in_tree_$tr.tre 
+~/soft/iqtree-2.2.0-Linux/bin/iqtree2 --alisim alignment_$tr -m GTR{uniform/uniform/uniform/uniform/uniform}+F{uniform/uniform/uniform/uniform}+G4{uniform} -t $file --length '$len' -af fasta -seed $myrand
+rm  $file
+done
+
+for tr in `seq ${SLURM_ARRAY_TASK_ID} $range`
+do
+cat in_tree_$tr.tre.log >> aln_${SLURM_ARRAY_TASK_ID}.log
+rm in_tree_$tr.tre.log
+cat alignment_$tr.fa >> aln_${SLURM_ARRAY_TASK_ID}.fa
+rm alignment_$tr.fa
+done
+date' >> $batch
+sbatch $batch
+
