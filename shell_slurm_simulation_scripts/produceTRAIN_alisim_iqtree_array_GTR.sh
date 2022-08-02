b6d371018843a0afe1fc78e90e08cc2f1259a6de
Anton <antony.suvorov@gmail.com>
2022-08-02 16:44:47 -0400

    update efficiency

--- shell_slurm_simulation_scripts/produceTRAIN_alisim_iqtree_array_GTR.sh
+++ shell_slurm_simulation_scripts/produceTRAIN_alisim_iqtree_array_GTR.sh
@@ -2,12 +2,11 @@
 trees=$1
 len=$2
 
-n=$( cat $trees | wc -l )
+split -d -l 1000 -a6 $trees chunk_ --additional-suffix=.tre --numeric-suffixes=1
+
 sed -n 1p "$trees" > fixed_tree.tre
-for i in `seq 1 $n`
-do 
-	sed -n "$i"p "$trees" > in_tree_"$i".tre
-done
+
+n=$(($(cat $trees | wc -l)/1000))
 
 batch=alisim_iqtree_array.sh
 
@@ -17,12 +16,15 @@ echo '#!/bin/bash
 #SBATCH --mem=1G
 #SBATCH -n 1
 #SBATCH -t 100:00:00
-#SBATCH --array=1-'"$n"':1000
+#SBATCH --array=1-'"$n"'
 #SBATCH --requeue' >> $batch
 
 echo 'date
-range=$((${SLURM_ARRAY_TASK_ID}+999))
-for tr in `seq ${SLURM_ARRAY_TASK_ID} $range`
+start=$((${SLURM_ARRAY_TASK_ID} * 1000 - 1000 + 1))
+stop=$((${SLURM_ARRAY_TASK_ID} * 1000))
+slurm_id=$(printf "%06d" ${SLURM_ARRAY_TASK_ID})
+split -d -l 1 -a6 chunk_$slurm_id.tre in_tree_ --additional-suffix=.tre --numeric-suffixes=$start
+for tr in `seq -f '%06g' $start $stop`
 do
 myrand=$((`shuf -i 0-4294967295 -n1`))
 file=in_tree_$tr.tre 
@@ -30,7 +32,7 @@ file=in_tree_$tr.tre
 rm  $file
 done
 
-for tr in `seq ${SLURM_ARRAY_TASK_ID} $range`
+for tr in `seq -f '%06g' $start $stop`
 do
 cat in_tree_$tr.tre.log >> aln_${SLURM_ARRAY_TASK_ID}.log
 rm in_tree_$tr.tre.log
