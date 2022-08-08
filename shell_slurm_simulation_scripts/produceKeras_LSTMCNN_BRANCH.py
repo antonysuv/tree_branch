b8bf8690642c4a95a073da193fe577129597839a
Anton <antony.suvorov@gmail.com>
2022-08-08 17:37:03 -0400

    LSTM

---
+++ shell_slurm_simulation_scripts/produceKeras_LSTMCNN_BRANCH.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+batch=keras_lstmcnn.sh
+
+if [ -e $batch ]
+then
+    continue
+fi
+echo '#!/bin/bash' > $batch
+echo '#SBATCH --job-name=lstmcnn' >> $batch
+echo '#SBATCH --ntasks=1' >> $batch
+echo '#SBATCH --cpus-per-task=1' >> $batch
+echo '#SBATCH --mem=60G' >> $batch
+echo '#SBATCH --time=100:00:00' >> $batch
+echo '#SBATCH --partition=volta-gpu' >> $batch
+echo '#SBATCH --output=lstmcnn-%j.log' >> $batch
+echo '#SBATCH --gres=gpu:1' >> $batch
+echo '#SBATCH --qos=gpu_access' >> $batch
+
+echo 'python3.9 ~/scripts/keras_LSTM_CNN_BRANCH.py --tr TRAIN.npy --te TEST.npy --trl TRAIN/train_simulation.Y.txt --tel TEST/test_simulation.Y.txt' >> $batch
+sbatch $batch
